a03b2ca3e6cb0db6b193ccd76edc464de96de379
1MikhailStepanov1 <wakamail923@gmail.com>
2023-06-18 18:22:10 +0300

    Added simple trip stats

--- src/main/java/backend/controllers/StatisticController.java
+++ src/main/java/backend/controllers/StatisticController.java
@@ -2,13 +2,21 @@ package backend.controllers;
 
 import backend.DTO.StatisticRequest;
 import backend.DTO.StatisticType;
+import backend.services.StatisticService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Date;
+
 @RestController("/statistic")
 public class StatisticController {
+
+    @Autowired
+    private StatisticService statisticService;
 
     @GetMapping
     public Object getStatistic(@RequestParam Long userId,
@@ -16,7 +24,11 @@ public class StatisticController {
                                @RequestParam String startTime,
                                @RequestParam String endTime,
                                @RequestParam StatisticType type) {
-        return null;
+        if (type == StatisticType.RESERVATIONS){
+            return statisticService.getReservationStats(userId, carId, Date.valueOf(startTime), Date.valueOf(endTime));
+        } else {
+            return statisticService.getTripStats(userId, carId, Date.valueOf(startTime), Date.valueOf(endTime));
+        }
     }
 
 }
--- src/main/java/backend/services/StatisticService.java
+++ src/main/java/backend/services/StatisticService.java
@@ -1,10 +1,15 @@
 package backend.services;
 
+import backend.entity.TripEntity;
+import backend.services.interfaces.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 
 @Service
 public class StatisticService {
@@ -18,11 +23,36 @@ public class StatisticService {
     @Autowired
     private UserServiceImpl userService;
 
+    @Autowired
+    private TripService tripService;
+
     public Map<String, Object> getReservationStats(Long userId, String carId, Date start, Date end) {
 
     }
 
     public Map<String, Object> getTripStats(Long userId, String carId, Date start, Date end) {
-
+        Map<String, Object> result = new HashMap<>();
+        AtomicReference<Long> tripCount = new AtomicReference<>(0L);
+        AtomicReference<Long> tripHours = new AtomicReference<>(0L);
+        Stream<TripEntity> tripStream = tripService.getTrips().stream();
+        if (userId != null){
+            tripStream.filter(tripEntity -> tripEntity.getDriverId() == userId);
+        }
+        if (carId != null){
+            tripStream.filter(tripEntity -> tripEntity.getCarId() == carId);
+        }
+        if (start != null){
+            tripStream.filter(tripEntity -> tripEntity.getStartTime().getTime() > start.getTime());
+        }
+        if (end != null){
+            tripStream.filter(tripEntity -> tripEntity.getEndTime().getTime() < end.getTime());
+        }
+        tripStream.forEach(tripEntity -> {
+            tripCount.updateAndGet(v -> v + 1);
+            tripHours.updateAndGet(v -> v + tripEntity.getEndTime().getTime() - tripEntity.getStartTime().getTime());
+        });
+        result.put("TripCount", tripCount);
+        result.put("TripHours", (double) tripHours.get() / 1000 / 60 / 60);
+        return result;
     }
 }
