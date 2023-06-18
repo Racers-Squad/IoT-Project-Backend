6ac6e9fdcf7c4e885940ec8aff9a26dcde5c0970
Danil Vakhrushev <danil1209@yandex.ry>
2023-06-18 17:26:08 +0300

    Added statistic classes

--- src/main/java/backend/services/StatisticService.java
+++ src/main/java/backend/services/StatisticService.java
@@ -1,7 +1,28 @@
 package backend.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Map;
 
 @Service
 public class StatisticService {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @Autowired
+    private CarServiceImpl carService;
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    public Map<String, Object> getReservationStats(Long userId, String carId, Date start, Date end) {
+
+    }
+
+    public Map<String, Object> getTripStats(Long userId, String carId, Date start, Date end) {
+
+    }
 }
