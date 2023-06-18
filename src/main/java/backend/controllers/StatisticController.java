3398403490c02453690fe8e0d8e30abdcff7bea9
Danil Vakhrushev <danil1209@yandex.ry>
2023-06-18 19:22:59 +0300

    Change requirements

--- src/main/java/backend/controllers/StatisticController.java
+++ src/main/java/backend/controllers/StatisticController.java
@@ -2,7 +2,9 @@ package backend.controllers;
 
 import backend.DTO.StatisticRequest;
 import backend.DTO.StatisticType;
+import backend.entity.UserEntity;
 import backend.services.StatisticService;
+import backend.services.UserServiceImpl;
 import backend.util.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +22,20 @@ public class StatisticController {
     @Autowired
     private StatisticService statisticService;
 
+    @Autowired
+    private UserServiceImpl userService;
+
     @GetMapping
-    public Object getStatistic(@RequestParam(required = false) Long userId,
+    public Object getStatistic(@RequestParam(required = false) String email,
                                @RequestParam(required = false) String carId,
                                @RequestParam(required = false) String startTime,
                                @RequestParam(required = false) String endTime,
                                @RequestParam StatisticType type) throws ParseException {
+        Long userId = null;
+        if (email != null) {
+            UserEntity user = userService.findByEmail(email);
+            userId = user.getId();
+        }
         if (type == StatisticType.RESERVATIONS){
             return statisticService.getReservationStats(userId, carId, CommonUtils.parseDate(startTime), CommonUtils.parseDate(endTime));
         } else {
